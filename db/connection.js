0e6191e9b81f8c9e8ec0c306e1c546504b419789
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-06-29 18:35:23 -0300

    fix: testando gihub

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: 'db',
-  user: 'root',
-  password: 'password',
-  database: 'StoreManager',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = connection;
