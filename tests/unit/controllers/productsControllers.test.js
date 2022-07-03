c03b79a3a1d0c034fda1ee1e38783efe91610c70
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-07-03 17:01:08 -0300

    feat: teste da camada service dos do disparo de erro dos req. 10 e 12

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -97,3 +97,63 @@ describe('quando a função é chamada', () => {
     })
   })
 })
+
+// Funcão update:
+describe('quando a função é chamada', () => {
+  describe('mas já existe um nome idêntico no banco', () => {
+    const req = {};
+    const res = {};
+    const next = sinon.spy();
+    const erro = {
+      status: 404,
+      message: 'Product not found',
+    };
+
+    beforeEach(() => {
+      req.params = 5;
+      req.body = 'bola';
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon.stub(service, 'updateProduct').throws(erro);
+    })
+
+    afterEach(() => {
+      service.updateProduct.restore();
+    })
+
+    it('é passado um erro no next', async () => {
+      await control.updateProduct(req, res, next);
+      expect(next.calledWith(erro)).to.be.equal(true);
+    })
+  })
+})
+
+// FUnção delete:
+
+describe('quando a função é chamada', () => {
+  describe('mas já existe um nome idêntico no banco', () => {
+    const req = {};
+    const res = {};
+    const next = sinon.spy();
+    const erro = {
+      status: 404,
+      message: 'Product not found',
+    };
+
+    beforeEach(() => {
+      req.params = 5;
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon.stub(service, 'deleteProduct').throws(erro);
+    })
+
+    afterEach(() => {
+      service.deleteProduct.restore();
+    })
+
+    it('é passado um erro no next', async () => {
+      await control.deleteProduct(req, res, next);
+      expect(next.calledWith(erro)).to.be.equal(true);
+    })
+  })
+})
